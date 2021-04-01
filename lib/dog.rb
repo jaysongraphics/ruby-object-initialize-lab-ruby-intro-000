8019748be380a53f1849251f3378bbaaebaada38
edward arias <jaysongraphics@gmail.com>
2021-04-01 15:33:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-        def initialize= (name, breed)
+        def initialize= 
          @name = name
          @breed= bread
         end
@@ -9,4 +9,10 @@ class Dog
               def breed= (bread)
                 @bread = bread
               end
+           def name
+             @name
+            end
+       def breed
+         @breed
+       end
 end
