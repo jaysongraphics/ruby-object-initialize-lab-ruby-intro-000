8e15febf713f9abff8706ad4375f11d57fc2cd65
edward arias <jaysongraphics@gmail.com>
2021-04-01 15:14:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,12 @@
 class Person
         def initialize(person)
         @person = person
-        end
+        
             def person= (person)
               @person = person
             end
         def person
           @person
+        end
         end
 end
