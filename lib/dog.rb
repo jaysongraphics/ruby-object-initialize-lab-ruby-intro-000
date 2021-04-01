2491d80267ee20f4edea3efc927ed234f72f757f
edward arias <jaysongraphics@gmail.com>
2021-04-01 15:15:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,11 @@
 class Dog
-  def initialize(dog)
-    @dog= dog
+    def initialize(dog)
+     @dog= dog
     end
-      def dog= (dog)
-      @dog= dog
-    end
+            def dog= (dog)
+               @dog= dog
+            end
      def dog
-     @dog
+       @dog
      end
-
 end
